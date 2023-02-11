aa4e80746658cb2108efd0a326ed68b2f5710521
rahulkmrsahu <rahulkmrsahu@gmail.com>
2023-02-11 05:51:21 +0530

    update

--- script.js
+++ script.js
@@ -41,6 +41,36 @@ function getWeatherReport(city) {
 
 function showWeatherReport(weather) {
     console.log(weather)
+
+    let city = document.getElementById("city")
+    city.innerText = `${weather.name},${weather.sys.country}`
+
+    let temperature = document.getElementById("temp")
+    temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`
+
+    let minMaxTemp = document.getElementById('min-max')
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max)`
+
+
+    let weatherType = document.getElementById('weather')
+    weatherType.innerText = `${weather.weather[0].main}`
+
+    let date = document.getElementById('date')
+    let todayDate = new Date();
+    date.innerText = dateManage(todayDate)
+
+
 }
 
 //Date manage
+function dateManage(dateArg) {
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"]
+    let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    let year = dateArg.getFullYear()
+    let month = months[dateArg.getMonth()]
+    let date = dateArg.getDate()
+    let day = days[dateArg.getDay()]
+
+    return `${date}${month}(${day}), ${year}`
+}
